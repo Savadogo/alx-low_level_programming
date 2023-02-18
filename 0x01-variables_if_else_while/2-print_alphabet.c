c1efaadb9e4d8ded15797d14c2f7faa34dc4ba10
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-18 05:51:12 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Print sign of number
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('a' + i);
+	}
+	putchar('\n')
+	return (0);
+}
