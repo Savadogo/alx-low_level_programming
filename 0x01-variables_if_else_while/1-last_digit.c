facaa77a6dfb8fb5ee68b69760e08b96eb7ee5bc
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-18 04:32:04 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
 /**
  * main - Print sign of number
  * Return: 0
  */
 int main(void)
 {
-	int n;
-	int last_digit;
-
+	int n,last_digit;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
