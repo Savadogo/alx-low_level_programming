ff378e12d36aad2d9176b6d44366b05cce38662e
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 11:56:03 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Print sign of number
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar('a' + i);
+	}
+	putchar('\n');
+	return (0);
+}
