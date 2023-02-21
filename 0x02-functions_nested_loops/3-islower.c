ee0754c0c8aa81af35e09edfaed30e3e86e491ef
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 14:41:57 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Returns 1 if c is lowercase and 0 otherwise
+ * Return - int
+ */
+
+int _islower(int c)
+{
+	int r = 0;
+
+	if(r>=97 && r<=122)
+	{
+		r = 1;
+	}
+	return (0);
+}
+
