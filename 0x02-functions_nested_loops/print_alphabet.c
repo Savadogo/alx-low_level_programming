224f805fee8bb1ea9a4069e0cc4d96251770287a
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 12:07:08 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,11 @@
+int print_alphabet(void)
+{
+        int i;
+
+        for (i = 0; i < 26; i++)
+        {
+                _putchar('a' + i);
+        }
+        _putchar('\n');
+        return (0);
+}
