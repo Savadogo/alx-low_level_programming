aed497889c3a6ab273e62a1837bbd590850b50dc
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 02:42:10 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Print sign of number
+ * Return: 0
+ */
+int main(void)
+{
+	char to_print[] = "_putchar";
+	int i;
+
+	for (i = 0; i < strlen(to_print) - 1; i++)
+	{
+		putchar(to_print[i]);
+	}
+	putchar('\n');
+	return (0);
+}
