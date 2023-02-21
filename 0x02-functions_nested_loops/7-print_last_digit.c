df43109706e74916974a1d22b4672bd7abd9fe80
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 15:49:20 -0800

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - function that give the last digit
+ * Return: the last digit
+ * @n: integer to test
+ */
+
+int print_last_digit(int n)
+{
+	return (n%10);
+}
+
