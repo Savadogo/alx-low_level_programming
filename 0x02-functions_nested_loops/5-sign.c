6684e29498c6110ddf77fa2f382061540deae5ae
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 15:26:50 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _print_sign - function that print sign of a number
+ * Return: the sign of the number
+ * @n: integer to test
+ */
+
+int print_sign(int n)
+{
+	int r ;
+
+	if (n > 0)
+	{
+		r = 1;
+		_putchar ("+");
+	}
+	else if (n < 0)
+	{
+		r = - 1;
+		_putchar ("-");
+	}
+	else
+	{
+		r = 0;
+		_putchar ("0");
+	}
+	return (r);
+}
+
