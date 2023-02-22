3d7e6b5e82e30486f28ec83d3216ff4da4cf5e5d
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 18:03:32 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_to_98 - function to go to 98 from an input number
+ * @n: input integer
+ */
+
+void add(int n)
+{
+	do
+	{
+		printf(n);
+		if (n < 98)
+		{
+			n++;
+			printf(", ");
+		}
+		else if (n > 98)
+		{
+			n--;
+			printf(", ");
+		}
+	}while (n != 98)
+}
+
