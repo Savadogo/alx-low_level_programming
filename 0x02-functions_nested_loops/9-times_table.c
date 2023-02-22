0bebbd8c84c429b587c78c9efc149df1509874cc
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 17:17:14 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * times_table - printing each minutes of the day
+ */
+
+void times_table(void)
+{
+	int i, j, m;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			m = i * j;
+			if (m >= 10)
+			{
+				_putchar((m / 10) + '0');
+			}
+			_putchar((m / 10) + '0');
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
+
