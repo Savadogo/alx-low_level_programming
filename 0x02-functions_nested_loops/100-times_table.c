670b547b9012b8aec76fe03965d7e1afa7a3514d
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 18:37:29 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,9 @@ void print_times_table(int n)
 	int i, j, m;
 	if (n > 0 && n < 15)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < n + 1; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < n + 1; j++)
 			{
 				m = i * j;
 				if (m >= 10)
@@ -19,7 +19,7 @@ void print_times_table(int n)
 					_putchar((m / 10) + '0');
 				}
 				_putchar((m % 10) + '0');
-				if (j < 9)
+				if (j < n)
 				{
 					if (i * (j + 1) >= 10)
 					{
