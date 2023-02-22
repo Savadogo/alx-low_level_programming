ff2a33772f920868940eca87ba42785eba8b3631
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 17:04:20 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,13 +9,14 @@ void jack_bauer(void)
 
 	for (i = 0; i < 24; i++)
 	{
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-		_putchar(':');
 		for (j = 0; j < 60; j++)
 		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((i % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
