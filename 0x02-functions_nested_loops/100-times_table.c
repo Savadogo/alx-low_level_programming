fd309ad6cb161656891b8da5e37e2cc24bb3b145
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-22 05:33:56 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,28 +27,17 @@ void print_times_table(int n)
 					_putchar('0' + m / 10);
 					_putchar('0' + m % 10);
 				}
-				else
-				{
-					_putchar('0' + m % 10);
-				}
+				_putchar('0' + m % 10);
 				if (j < n)
 				{
-					if (i * (j + 1) >= 100)
+					_putchar(',');
+					_putchar(' ');
+					if (i * (j + 1) >= 10)
 					{
-						_putchar(',');
 						_putchar(' ');
 					}
-					else if ((i * (j + 1) >= 10) && (i * (j + 1) < 100))
+				       	else if (i * (j + 1) >= 100)
 					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
-				       	else
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
 						_putchar(' ');
 					}
 				}
