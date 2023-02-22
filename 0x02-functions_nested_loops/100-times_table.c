0dc31d0ae9ac0a83d9695d00c68f36fbbbcb16e6
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 18:32:04 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * times_table - printing each minutes of the day
+ * @n: size of the table
+ */
+
+void times_table(int n)
+{
+	int i, j, m;
+
+	if (n < 0 || n > 15)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				m = i * j;
+				if (m >= 10)
+				{
+					_putchar((m / 10) + '0');
+				}
+				_putchar((m % 10) + '0');
+				if (j < 9)
+				{
+					if (i * (j + 1) >= 10)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+					else
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
