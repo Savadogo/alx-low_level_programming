21f2bf4111443567b29fda97fa73edf6132de7aa
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-21 17:27:03 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,8 +16,20 @@ void times_table(void)
 			{
 				_putchar((m / 10) + '0');
 			}
-			_putchar((m / 10) + '0');
-			_putchar(',');
+			_putchar((m % 10) + '0');
+			if (j < 9)
+			{
+				if (i * (j+1) >=10)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar('  ');
+				}
+			}
 		}
 		_putchar('\n');
 	}
