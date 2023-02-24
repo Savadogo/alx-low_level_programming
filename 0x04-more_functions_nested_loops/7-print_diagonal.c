8ef8a71b03119bbba68fba654567de93ef406aa7
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-23 18:01:00 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - Print a diagonal with a number of characters
+ * @n : number of caracter to use
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
+
