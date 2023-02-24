db253edea5b441b81631342aa98bfa87ccc8ea71
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-23 17:50:47 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Print a line with a number of characters
+ * @n : number of lines to use
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
+
