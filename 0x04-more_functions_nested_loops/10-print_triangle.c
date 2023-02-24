3476fb924466b67d8c6a131b2af6fb301cdd7eec
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-23 18:39:22 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - Print a trinagle with a number of characters
+ * @n : number of caracter to use
+ */
+
+void print_triangle(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = n - i - 1; j < n; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
+
