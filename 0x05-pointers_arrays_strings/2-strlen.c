25c48b32116cbc732404c0c101c74e7bd7bebf7e
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-27 12:08:11 -0800

    2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _strlen - calculate length of a string
- * @s string to find the length: 
+ * @s: string to find the length
+ * Return: n giving the size of the string
  */
-
 int _strlen(char *s)
 {
 	int n;
-	
+
 	n = 0;
 	while (*(s + n) != '\0')
 	{
