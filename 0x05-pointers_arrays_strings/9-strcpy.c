8069518fd0cce3d475ba1b3b23c41de225d47bdf
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-27 17:03:21 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return dest;
+}
+
