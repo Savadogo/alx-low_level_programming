062c8fc2c87e4ad85e405eba9f0c05fc7121cc46
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-27 16:10:08 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - fucntion that print a string
+ * @str: string to print
+ */
+void puts2(char *str)
+{
+	int n;
+
+	n = 0;
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+		if (*(str + n + 1) != '\0')
+		{
+			break;
+		}
+		n = n + 2;
+	}
+	_putchar('\n');
+}
+
