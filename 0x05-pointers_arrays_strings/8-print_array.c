e211581b575195aa09f36a90ef5b07e572c103bf
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-02-27 16:34:26 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - fucntion that print a string
+ * @a: array to print
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		_putchar(*(a + i) + '0');
+		i++;
+	}
+
+	_putchar('\n');
+}
+
