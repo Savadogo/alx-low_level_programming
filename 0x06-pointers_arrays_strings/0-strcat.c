8d02c988e583b139d0a09b6e0106d5d376cf4ee6
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-01 09:12:55 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - This function appends the src string to the dest string
+ * Return: a pointer to the resulting string dest
+ * @dest: string to add to
+ * @src: strng to add
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n, m, i;
+
+	n = 0;
+	m = 0;
+	while (*(dest + n) != '\0')
+	{
+		n++;
+	}
+	while (*(src + m) != '\0')
+	{
+		m++;
+	}
+	for (i = 0; i <= m; i++)
+	{
+		*(dest + n + i + 1) = *(src + n);
+	}
+	return (dest);
+}
+
---
+++ 0x06-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - fucntion that print a string
+ * @str: string to print
+ */
+void puts_half(char *str)
+{
+	int n, m;
+
+	n = 0;
+	while (*(str + n) != '\0')
+	{
+		n++;
+	}
+	if (n % 2 == 0)
+	{
+		m = n / 2;
+	}
+	else
+	{
+		m = (n - 1) / 2 + 1;
+	}
+	while (m < n)
+	{
+		_putchar(*(str + m));
+		m++;
+	}
+
+	_putchar('\n');
+}
+
