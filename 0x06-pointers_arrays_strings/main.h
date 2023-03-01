2b924145eb9f1e5aa9c9810aa538851d6500879f
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-01 11:33:06 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
