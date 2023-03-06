77e628182c67a90460ecf29c9d1614194745dc8d
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-06 13:59:21 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy - This function appends the src string to the dest string
+ * Return: a pointer to the resulting string dest
+ * @dest: string to add to
+ * @src: strng to add
+ * @n: number of caracter to copy
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int m;
+
+	for (m = 0; m < n; m++)
+	{
+		*(dest + m) = *(src + m);
+	}
+	return (dest);
+}
+
