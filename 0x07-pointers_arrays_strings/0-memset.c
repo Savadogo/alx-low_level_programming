0017ca54f5663902c458ed48f3d52bb38cad419a
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-06 13:53:43 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _memcpy - This function appends the src string to the dest string
+ * _memset - This function appends the src string to the dest string
  * Return: a pointer to the resulting string dest
  * @s: char where to copy
  * @b: char to copy
  * @n: n bytes where to cpy src
  */
 
-char *_memcpy(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	while (n > 0)
 	{
