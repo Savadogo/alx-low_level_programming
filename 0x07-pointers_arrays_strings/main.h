1e859973d80bc9b22845f58e9e50d54b469d3cad
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-06 16:21:49 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-void reverse_array(int *a, int n);
+char *_strpbrk(char *s, char *accept);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
