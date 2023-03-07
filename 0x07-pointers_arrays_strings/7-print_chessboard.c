dbe9911352e15496642cd4b9355fabe221ba7280
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-06 17:12:50 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - fucntion that print a string
+ * @a: array to print
+ */
+void print_chessboard(int (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0;  < 8; j++)
+		{
+			_putchar(*(*(a + i) + j));
+			j++;
+		}
+		i++;
+	}
+}
+
