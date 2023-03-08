d82c8f751154f0739f6423c36d226b19391d828c
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-08 12:23:30 -0800

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,14 +6,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		return (_puts_recursion(s + 1));
+		_print_rev_recursion(s + 1);
 	}
 	_putchar(*s);
-	_puts_recursion(s - 1);
 }
