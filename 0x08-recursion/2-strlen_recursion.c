58533caf6dd16d7ba483305ca7022ace09f94a1c
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-08 13:21:20 -0800

    2-strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,9 +7,10 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return (1);
+		return (_strlen_recursion(s + 1) + 1);
+	
 	}
-	return (_strlen_recursion(s) + 1);
+	return (0);
 }
