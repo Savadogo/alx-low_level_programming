189bccc1c5a4b62cea0e72721a2360882fb575e7
Savadogo <savadogoilassa@gmail.com>
2023-03-10 23:34:18 +0100

    Add files via upload

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function that give the absolute vale of a number
+ * Return: the absolute value
+ * @n: integer to test
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
+
