3a45039f2c968461482a7a8cc2b663c54d861233
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-12 11:04:44 -0700

    testing

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,17 @@
+#include "main.h"
+/**
+ * _puts - fucntion that print a string
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int n;
 
+	n = 0;
+	while (*(str + n) != '\0')
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
