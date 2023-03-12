bef6b6115b8f2a21c9e8605d277e50281534988e
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-12 16:41:18 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ * @argc: number of arguments
+ * @argv: list of arguments
+ */
+int main(int argc, char *argv[])
+{
+	puts(*argv);
+	return (0);
+}
+
