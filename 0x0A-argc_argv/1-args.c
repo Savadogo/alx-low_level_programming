476c8534c97184625306d3f9d9d3febf7257b5df
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-12 16:56:10 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ * @argc: number of arguments
+ * @argv: list of arguments
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc);
+	return (0);
+}
