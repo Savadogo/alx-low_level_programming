a5c2024fdb71ff313a203a21bd52150dd05ab2a2
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-13 13:06:03 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * _strdup - allocates a string to a ponter
+ * Return: a pointer to the allocated memory
+ * @str: sring to put
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	unsigned int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	a = malloc(size * strlen(str));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < strlen(str); i++)
+	{
+		a[i] = str[i];
+	}
+	return (a);
+}
