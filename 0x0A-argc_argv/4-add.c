45cfb85d1a67a17cfb863825d30dc81c56b3bf47
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-12 17:45:39 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: 0
+ * @argc: number of arguments
+ * @argv: list of arguments
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		puts("0");
+		return (0);
+	}
+	else
+	{
+		int i, agg;
+
+		agg = 0;
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(*(argv + i)) != 0)
+			{
+				agg += atoi(*(argv + i));
+			}
+			else
+			{
+				puts("Error");
+				return (0);
+			}
+		}
+		printf("%d\n", agg);
+	}
+	return (0);
+}
