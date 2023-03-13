fb1bc9e72302b76dbd146be2e4e5a34191eb41f0
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-13 13:39:05 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,28 +12,36 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *a;
-	unsigned int i;
+	unsigned int i, size1, size2;
 
 	if (s1 == NULL)
 	{
-		*s1 = "";
+		size1 = 0;
+	}
+	else
+	{
+		size1 = strlen(s1);
 	}
 	if (s2 == NULL)
 	{
-		*s2 = "";
+		size2 = 0;
 	}
-	a = malloc(strlen(s1) +strlen(s2)  + 1);
+	else
+	{
+		size2 = strlen(s2);
+	}
+	a = malloc(size1 + size2  + 1);
 	if (a == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < strlen(s1); i++)
+	for (i = 0; i < size1; i++)
 	{
 		a[i] = s1[i];
 	}
-	for (i = 0; i < strlen(s2); i++)
+	for (i = 0; i < size2; i++)
 	{
-		a[i + strlen(s1)] = s2[i];
+		a[i + size1] = s2[i];
 	}
 	return (a);
 }
