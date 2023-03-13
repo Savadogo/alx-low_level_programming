063c58e21696abc14bbcca3ffd5c6d9dc482838d
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-12 18:48:30 -0700

    pushing

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -24,9 +24,9 @@ int main(int argc, char *argv[])
 		{
 			long unsigned int j;
 
-			for (j = 0; j < strlen(*(argv + i)); j++)
+			for (j = 0; j < strlen(argv[i]); j++)
 			{
-				if (*(argv + i)[j] < '0' || *(argv + i)[j] > '9')
+				if (argv[i][j] < '0' || argv[i][j] > '9')
 				{
 					puts("Error");
 					return (1);
