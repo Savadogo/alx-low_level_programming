97b193f751ef5ce8fa8886ae254a674d3a0377a0
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-12 17:17:13 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ * @argc: number of arguments
+ * @argv: list of arguments
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		puts("Error\n");
+		return (1);
+	}
+	else
+	{
+		int x, y;
+
+		x = *argv[1];
+		y = *argv[2];
+		printf("%d\n", x * y);
+	}
+	return (0);
+}
