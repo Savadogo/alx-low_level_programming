15d9d09467485914cf234b375183f47844f8b9cb
Ilassa Savadogo <savadogoilassa@gmail.com>
2023-03-13 14:35:52 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,9 +26,13 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		a[i] = malloc(width * sizeof(int));
+		a[i] = calloc(width, sizeof(int));
 		if (a[i] == NULL)
 		{
+			for (j = 0; j < i; j++)
+			{
+				free(a[j]);
+			}
 			free(a);
 			return (NULL);
 		}
